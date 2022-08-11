fef78ee359b777448737a8224d3283340161a59f
ffiza <fgiza@protonmail.com>
2022-08-11 13:01:54 -0300

    Update notebook, configuration parameters and output figures.

--- code/tp1_settings.py
+++ code/tp1_settings.py
@@ -1,9 +1,17 @@
+# ----------------------------------------------------------------------------
+# Title:   ANN configuration parameters
+# ----------------------------------------------------------------------------
+
 N_SIGNALS = 17
 TEST_SIZE = 0.2
-N_LAYERS = 3
-N_NEURONS = [64, 32, 1]
-ACTIVATION_FUNCTIONS = ['tanh', 'tanh', 'relu']
-N_EPOCHS = 30
+N_LAYERS = {'radar': 3,
+            'cems': 3}
+N_NEURONS = {'radar': [64, 32, 1],
+             'cems': [64, 32, 1]}
+ACTIVATION_FUNCTIONS = {'radar': ['tanh', 'tanh', 'relu'],
+                        'cems': ['tanh', 'tanh', 'relu']}
+N_EPOCHS = {'radar': 40,
+            'cems': 20}
 VALIDATION_SPLIT = 0.2
 LOSS_FUNCTION = 'binary_crossentropy'
 OPTIMIZER = 'rmsprop'
