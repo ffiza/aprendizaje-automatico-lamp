fea05802fbb8b22fa3606341ab0931eb1a21f5ed
ffiza <fgiza@protonmail.com>
2022-08-11 16:17:35 -0300

    Update notebook and configuration file for TP 2.

--- code/tp2_settings.py
+++ code/tp2_settings.py
@@ -1,3 +1,15 @@
 # ----------------------------------------------------------------------------
 # Title:   Configuration parameters
 # ----------------------------------------------------------------------------
+
+TEST_SIZE = 0.2
+N_STEPS_IN = 5
+N_STEPS_OUT = 3
+N_LAYERS = 2
+N_NEURONS = 50
+ACTIVATION_FUNCTION = 'tanh'
+OPTIMIZER = 'adam'
+LOSS_FUNCTION = 'mse'
+N_EPOCHS = 5
+VALIDATION_SPLIT = 0.2
+VERBOSE_TYPE = 0
