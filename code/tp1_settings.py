20a7a2bc0ef805aaadb1fb696c7df3624345ae0c
ffiza <fgiza@protonmail.com>
2022-08-11 09:54:47 -0300

    Add further variables con configuration file.

--- code/tp1_settings.py
+++ code/tp1_settings.py
@@ -1,7 +1,9 @@
 N_SIGNALS = 17
 TEST_SIZE = 0.2
 N_LAYERS = 3
-N_NEURONS = [64, 31, 1]
+N_NEURONS = [64, 32, 1]
 ACTIVATION_FUNCTIONS = ['tanh', 'tanh', 'relu']
 N_EPOCHS = 30
 VALIDATION_SPLIT = 0.2
+LOSS_FUNCTION = 'binary_crossentropy'
+OPTIMIZER = 'rmsprop'
