9dc96e00f9b712ab7556ebaf2d915b07d29ed7ca
ffiza <fgiza@protonmail.com>
2022-08-12 11:11:34 -0300

    Divide notebook for TP 1 in two parts.

---
+++ code/tp1_radar_settings.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------------------------
+# Title:   Configuration parameters
+# ----------------------------------------------------------------------------
+
+N_SIGNALS = 17
+TEST_SIZE = 0.2
+N_LAYERS = 3
+N_NEURONS = [64, 32, 1]
+ACTIVATION_FUNCTIONS = ['tanh', 'tanh', 'relu']
+N_EPOCHS = 40
+VALIDATION_SPLIT = 0.2
+LOSS_FUNCTION = 'binary_crossentropy'
+OPTIMIZER = 'rmsprop'
