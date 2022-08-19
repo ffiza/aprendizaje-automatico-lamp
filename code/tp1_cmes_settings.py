e035cfce077a8888504637042b35c3ce70f7a268
ffiza <fgiza@protonmail.com>
2022-08-18 21:20:55 -0300

    Update part 2 of TP1.

--- code/tp1_cmes_settings.py
+++ code/tp1_cmes_settings.py
@@ -4,10 +4,11 @@
 
 N_SIGNALS = 17
 TEST_SIZE = 0.2
-N_LAYERS = 3
-N_NEURONS = [64, 32, 1]
-ACTIVATION_FUNCTIONS = ['tanh', 'tanh', 'relu']
-N_EPOCHS = 40
+N_LAYERS = 4
+N_NEURONS = [64, 32, 16, 1]
+ACTIVATION_FUNCTIONS = ['tanh', 'tanh', 'tanh', 'relu']
+N_EPOCHS = 100
 VALIDATION_SPLIT = 0.2
 LOSS_FUNCTION = 'binary_crossentropy'
 OPTIMIZER = 'rmsprop'
+VERBOSE = 0
