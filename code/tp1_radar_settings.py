feb394b7c3c23881f08cb582ee7332ef7dcc6f14
ffiza <fgiza@protonmail.com>
2022-08-18 21:20:32 -0300

    Update TP1: add comments, merge plots and consider different models.

--- code/tp1_radar_settings.py
+++ code/tp1_radar_settings.py
@@ -4,10 +4,43 @@
 
 N_SIGNALS = 17
 TEST_SIZE = 0.2
-N_LAYERS = 3
-N_NEURONS = [64, 32, 1]
-ACTIVATION_FUNCTIONS = ['tanh', 'tanh', 'relu']
-N_EPOCHS = 40
 VALIDATION_SPLIT = 0.2
 LOSS_FUNCTION = 'binary_crossentropy'
 OPTIMIZER = 'rmsprop'
+
+# Definition of the parameters of each model
+models_params = {'m0': {'N_LAYERS':             3,
+                        'N_NEURONS':            [64, 32, 1],
+                        'ACTIVATION_FUNCTIONS': ['tanh', 'tanh', 'relu'],
+                        'N_EPOCHS':             20,
+                        'COLOR':                'tab:blue'},
+                 'm1': {'N_LAYERS':             3,
+                        'N_NEURONS':            [64, 32, 1],
+                        'ACTIVATION_FUNCTIONS': ['tanh', 'tanh', 'relu'],
+                        'N_EPOCHS':             40,
+                        'COLOR':                'tab:orange'},
+                 'm2': {'N_LAYERS':             4,
+                        'N_NEURONS':            [64, 32, 16, 1],
+                        'ACTIVATION_FUNCTIONS': ['tanh', 'tanh', 'tanh',
+                                                 'relu'],
+                        'N_EPOCHS':             40,
+                        'COLOR':                'tab:green'},
+                 'm3': {'N_LAYERS':             4,
+                        'N_NEURONS':            [64, 32, 16, 1],
+                        'ACTIVATION_FUNCTIONS': ['tanh', 'tanh', 'relu',
+                                                 'relu'],
+                        'N_EPOCHS':             40,
+                        'COLOR':                'tab:red'},
+                 'm4': {'N_LAYERS':             5,
+                        'N_NEURONS':            [64, 32, 16, 8, 1],
+                        'ACTIVATION_FUNCTIONS': ['tanh', 'tanh', 'relu',
+                                                 'relu', 'relu'],
+                        'N_EPOCHS':             40,
+                        'COLOR':                'tab:purple'},
+                 'm5': {'N_LAYERS':             5,
+                        'N_NEURONS':            [64, 32, 16, 8, 1],
+                        'ACTIVATION_FUNCTIONS': ['tanh', 'tanh', 'relu',
+                                                 'relu', 'relu'],
+                        'N_EPOCHS':             60,
+                        'COLOR':                'tab:brown'},
+                 }
